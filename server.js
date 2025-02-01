a25864671dac95317fe0afba4944caba25e3525d
Chowiya <nd1553410@gmail.com>
2025-02-01 15:41:43 +0530

    done

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const app = express();
 app.use(cors(
     {
         origin:'https://home4paws.netlify.app',
+        methods: ['GET', 'POST','PUT','DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true
     }
 ));
